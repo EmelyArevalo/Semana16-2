cd234171949151225af3003deac884b4ceb96289
Michelle Arèvalo <emelymichellearlo@gmail.com>
2022-12-04 22:40:42 -0600

    Agregar archivos de proyecto.

---
+++ Semana16/Data/Semana16Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semana16.Data
+{
+    public class Semana16_ontext : DbContext
+    {
+        public Semana16Context(DbContextOptions<Semana16Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Teather> Teather { get; set; }
+    }
+}
